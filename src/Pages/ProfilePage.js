d2ed9ac2dc5054fe18f6705751c06705220c674e
ritik sharma <bharadwajritik1@gmail.com>
2023-03-20 03:23:00 +0530

    "edit user details"

--- src/Pages/ProfilePage.js
+++ src/Pages/ProfilePage.js
@@ -1,8 +1,36 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./ProfilePage.css";
 export default function ProfilePage() {
-  const name = useRef("");
-  const url = useRef("");
+  
+const [preVal,setPreVal]=useState([])
+const name = useRef('');
+const url = useRef('');
+
+
+
+    useEffect(() => {
+     async function fetchData(){
+    const res = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyBidV1BXfH0QkFRbCo9RYeo2zXEHNTZVWg", {
+      method: "POST",
+      body: JSON.stringify({
+        idToken: localStorage.getItem("Token")
+      }),
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    // console.log(data)
+    setPreVal(data.users[0].providerUserInfo[0])
+    
+}
+fetchData()
+    }, [])
+    console.log(preVal)
+   
+ 
+   
   const handleSubmit = async (e) => {
     e.preventDefault();
     const nameVal = name.current.value;
@@ -31,9 +59,9 @@ export default function ProfilePage() {
       <h2 className="my-3">Contact Details</h2>
       <form onSubmit={handleSubmit}>
         <label>Full Name:</label>
-        <input ref={name} req></input>
+        <input ref={name} value={preVal.displayName} req></input>
         <label>Profile Photo Url:</label>
-        <input ref={url} req></input>
+        <input ref={url} value={preVal.photoUrl} req></input>
         <button className="button-10" type="submit">
           Update
         </button>
